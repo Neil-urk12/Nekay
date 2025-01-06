c7025315339021056a41e892b270aed4bb6dafb6
Neil Vallecer <neilvallecer12@gmail.com>
2025-01-06 19:09:35 +0800

    refactor: Update Home.vue and vite.config.ts for improved structure and performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,
+  },
   build: {
     outDir: "dist",
     assetsDir: "assets",
     rollupOptions: {
       input: {
-        app: "./index.html",
+        main: path.resolve(__dirname, "index.html"),
         "service-worker": "./public/service-worker.js",
       },
     },
   },
+  optimizeDeps: {
+    include: ['vue']
+  },
   publicDir: "public",
-});
+})
