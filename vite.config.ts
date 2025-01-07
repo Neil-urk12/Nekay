d181d99961cd4b87504685dcbad0c452f811ac58
Neil Vallecer <neilvallecer12@gmail.com>
2025-01-07 12:45:41 +0800

    feat: Add Workbox for service worker generation and caching strategies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,67 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import path from 'path'
+import { generateSW } from 'workbox-build'
 
+const generateSWConfig = {
+  swDest: './dist/service-worker.js',
+  globDirectory: path.resolve(__dirname, "dist"),
+  globPatterns: ['**/*.{html,js,css,png,jpg,jpeg,gif,svg,webp,wav,mp3,json}'],
+  runtimeCaching: [
+    {
+      urlPattern: /\/api\//,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: "api-cache-v1",
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24,
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|gif|mp3|wav|webp|json)/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: "assets-cache-v1",
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24,
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:js|css|html)$/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: "static-resources-cache-v1",
+      }
+    }
+  ]
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: "vite-plugin-workbox",
+      closeBundle: async () => {
+        try {
+          await generateSW({
+            ...generateSWConfig,
+            runtimeCaching: generateSWConfig.runtimeCaching.map(entry => ({
+              ...entry,
+              handler: entry.handler as 'NetworkFirst' | 'CacheFirst' | 'StaleWhileRevalidate'
+            }))
+          })
+          console.log("Service worker generated successfully!")
+      
+        } catch ( err ) {
+          console.error('Service worker generation failed: ', err)
+        }
+      }
+    }
+  ],
   server: {
     host: true,
   },
@@ -14,7 +71,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, "index.html"),
-        "service-worker": "./public/service-worker.js",
       },
     },
   },
