467ed334794c58eca1d6869e09cc42afd1863aa9
Neil Vallecer <neilvallecer12@gmail.com>
2025-01-09 01:16:43 +0800

    refactor: Clean up router configuration and add folders route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,57 +1,69 @@
-import { createRouter, createWebHistory } from 'vue-router' 
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/login',
-      name: 'Login',
-      component: () => import('../views/Login.vue'),
+      path: "/login",
+      name: "Login",
+      component: () => import("../views/Login.vue"),
       meta: {
         hideBottomNav: true,
-        requiresAuth: false
-      }
+        requiresAuth: false,
+      },
     },
-    { 
-      path: '/',
-      component: () => import('../views/Home.vue'),
+    {
+      path: "/",
+      component: () => import("../views/Home.vue"),
+      meta: { requiresAuth: false },
+    },
+    {
+      path: "/home",
+      redirect: "/",
+      meta: { requiresAuth: false },
+    },
+    {
+      path: "/pomodoro",
+      component: () => import("../views/Pomodoro.vue"),
+      meta: { requiresAuth: false },
+    },
+    {
+      path: "/folders",
+      name: "Folders",
+      component: () => import("../views/FoldersListView.vue"),
       meta: { requiresAuth: false }
     },
-    { 
-      path: '/home', 
-      redirect: '/',
-      meta: { requiresAuth: false }
+    {
+      path: "/folders/:id",
+      name: "FolderTasks",
+      component: () => import("../views/FoldersTaskView.vue"),
+      meta: { requiresAuth: false },
     },
-    { 
-      path: '/pomodoro',
-      component: () => import('../views/Pomodoro.vue'),
-      meta: { requiresAuth: false }
+    // {
+    //   path: "/tasks",
+    //   component: () => import("../views/Tasks.vue"),
+    //   meta: { requiresAuth: false },
+    // },
+    {
+      path: "/journal",
+      component: () => import("../views/Journal.vue"),
+      meta: { requiresAuth: false },
     },
-    { 
-      path: '/tasks', 
-      component: () => import('../views/Tasks.vue'),
-      meta: { requiresAuth: false }
-    },
-    { 
-      path: '/journal', 
-      component: () => import('../views/Journal.vue'),
-      meta: { requiresAuth: false }
-    }
-  ]
-})
+  ],
+});
 
 // Navigation guard
 router.beforeEach((to, _from, next) => {
-  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true'
-  const requiresAuth = to.meta.requiresAuth
+  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
+  const requiresAuth = to.meta.requiresAuth;
 
   if (requiresAuth && !isAuthenticated) {
-    next('/login')
-  } else if (to.path === '/login' && isAuthenticated) {
-    next('/')
+    next("/login");
+  } else if (to.path === "/login" && isAuthenticated) {
+    next("/");
   } else {
-    next()
+    next();
   }
-})
+});
 
-export default router
+export default router;
