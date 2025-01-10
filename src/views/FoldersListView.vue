841368581b50f96d5fd78d51fd9f5f056603a6e5
Neil Vallecer <neilvallecer12@gmail.com>
2025-01-10 17:30:08 +0800

    style: Update button icons and input styles for improved UI consistency

--- src/views/FoldersListView.vue
+++ src/views/FoldersListView.vue
@@ -113,21 +113,57 @@ onMounted(() => {
 
         <div class="folder-actions">
           <template v-if="editingFolder?.id === folder.id">
-            <button class="icon-btn" @click.stop="saveEdit">✅</button>
-            <button class="icon-btn" @click.stop="cancelEdit">❌</button>
+            <button class="icon-btn" @click.stop="saveEdit"><svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              width="1.2rem"
+            >
+              <path
+                fill="#63E6BE"
+                d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
+              />
+            </svg></button>
+            <button class="icon-btn" @click.stop="cancelEdit"><svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+              width="1.2rem"
+            >
+              <path
+                fill="#f66151"
+                d="M378.4 71.4c8.5-10.1 7.2-25.3-2.9-33.8s-25.3-7.2-33.8 2.9L192 218.7 42.4 40.6C33.9 30.4 18.7 29.1 8.6 37.6S-2.9 61.3 5.6 71.4L160.7 256 5.6 440.6c-8.5 10.2-7.2 25.3 2.9 33.8s25.3 7.2 33.8-2.9L192 293.3 341.6 471.4c8.5 10.2 23.7 11.5 33.8 2.9s11.5-23.7 2.9-33.8L223.3 256l155-184.6z"
+              />
+            </svg></button>
           </template>
           <template v-else>
             <button
               class="icon-btn"
               @click.stop="editingFolder = { id: folder.id, name: folder.name }"
             >
-              ✏️
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            width="1.2rem"
+          >
+            <path
+              fill="#B197FC"
+              d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z"
+            />
+          </svg>
             </button>
             <button
               class="icon-btn"
               @click.stop="deleteConfirm = { id: folder.id, name: folder.name }"
             >
-              🗑️
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            width="1.2rem"
+          >
+            <path
+              fill="#a51d2d"
+              d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"
+            />
+          </svg>
             </button>
           </template>
         </div>
@@ -218,7 +254,8 @@ onMounted(() => {
 .edit-input {
   width: 100%;
   padding: 0.5rem;
-  border: 1px solid var(--primary-color);
+  border: 1px solid palevioletred;
+  background: none;
   border-radius: 4px;
   font-size: 1rem;
 }
@@ -265,5 +302,11 @@ onMounted(() => {
   border: none;
   padding: 0.5rem 1rem;
   cursor: pointer;
+}
+.icon-btn {
+  background: none;
+  padding: 0.25rem 0.5rem;
+  border: none;
+  border-radius: 0;
 }
 </style>
