890565dbb713f4c760502200ebb301531b7830dc
Neil Vallecer <neilvallecer12@gmail.com>
2025-01-10 15:31:54 +0800

    feat: Add background store to manage dynamic background images based on time of day

---
+++ src/stores/backgroundStore.ts
@@ -0,0 +1,33 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import { TimeOfDay } from "../views/Home.vue";
+
+export const useBackgroundStore = defineStore("background", () => {
+  const backgroundImage = ref("");
+
+  const setBackground = (url: string) => (backgroundImage.value = url);
+  const timeOfDay = ref<TimeOfDay>("morning");
+
+  const determineTimeOfDay = () => {
+    const hour = new Date().getHours();
+
+    if (hour >= 5 && hour < 12) {
+      timeOfDay.value = "morning";
+      setBackground("url(https://s6.imgcdn.dev/pSKhw.png)");
+    } else if (hour >= 12 && hour < 13) {
+      timeOfDay.value = "noon";
+      setBackground("url(https://s6.imgcdn.dev/pSsaL.jpg)");
+    } else if (hour >= 13 && hour < 18) {
+      timeOfDay.value = "afternoon";
+      setBackground("url(https://s6.imgcdn.dev/pSimB.png)");
+    } else if (hour >= 18 && hour < 20) {
+      timeOfDay.value = "evening";
+      setBackground("url(https://s6.imgcdn.dev/pSyWa.jpg)");
+    } else {
+      timeOfDay.value = "night";
+      setBackground("url(https://s6.imgcdn.dev/pSnuu.gif)");
+    }  
+  };
+
+  return { backgroundImage, setBackground, determineTimeOfDay, timeOfDay };
+});
