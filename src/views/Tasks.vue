17a7835b4bf83deb672bf7bb10f0fe3ff035de47
Neil Vallecer <neilvallecer12@gmail.com>
2025-01-10 11:45:50 +0800

    refactor: Improve checkbox styling and layout in task management UI

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -74,7 +74,16 @@ onMounted(() => {
           class="input-field"
         />
         <button @click="addTask" class="btn-primary">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="1rem"><path fill="#ffffff" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"/></svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            width="1rem"
+          >
+            <path
+              fill="#ffffff"
+              d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"
+            />
+          </svg>
           Add Task
         </button>
       </div>
@@ -87,21 +96,21 @@ onMounted(() => {
         class="task-item"
         :class="{ completed: task.completed }"
       >
-      <label class="checkbox-container">
-        <input
-          type="checkbox"
-          :checked="task.completed"
-          @change="toggleTask(task)"
-        />
-        <span class="checkmark"></span>
-      </label>
         <div class="task-content">
+          <div class="checkbox-container">
+            <input
+              type="checkbox"
+              :checked="task.completed"
+              @change="toggleTask(task)"
+            />
+            <span class="checkmark"></span>
+          </div>
           <template v-if="editingTask?.id === task.id">
             <input
               v-model="editingTask.content"
               @keyup.enter="saveEdit"
               @keyup.esc="editingTask = null"
-              class="edit-input input-field"
+              class="edit-input"
               @click.stop
             />
           </template>
@@ -115,10 +124,28 @@ onMounted(() => {
         <div class="task-actions">
           <template v-if="editingTask?.id === task.id">
             <button class="icon-btn" @click.stop="saveEdit">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="1.2rem"><path fill="#63E6BE" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                width="1.2rem"
+              >
+                <path
+                  fill="#63E6BE"
+                  d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
+                />
+              </svg>
             </button>
             <button class="icon-btn" @click.stop="editingTask = null">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="1.2rem"><path fill="#f66151" d="M378.4 71.4c8.5-10.1 7.2-25.3-2.9-33.8s-25.3-7.2-33.8 2.9L192 218.7 42.4 40.6C33.9 30.4 18.7 29.1 8.6 37.6S-2.9 61.3 5.6 71.4L160.7 256 5.6 440.6c-8.5 10.2-7.2 25.3 2.9 33.8s25.3 7.2 33.8-2.9L192 293.3 341.6 471.4c8.5 10.2 23.7 11.5 33.8 2.9s11.5-23.7 2.9-33.8L223.3 256l155-184.6z"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 384 512"
+                width="1.2rem"
+              >
+                <path
+                  fill="#f66151"
+                  d="M378.4 71.4c8.5-10.1 7.2-25.3-2.9-33.8s-25.3-7.2-33.8 2.9L192 218.7 42.4 40.6C33.9 30.4 18.7 29.1 8.6 37.6S-2.9 61.3 5.6 71.4L160.7 256 5.6 440.6c-8.5 10.2-7.2 25.3 2.9 33.8s25.3 7.2 33.8-2.9L192 293.3 341.6 471.4c8.5 10.2 23.7 11.5 33.8 2.9s11.5-23.7 2.9-33.8L223.3 256l155-184.6z"
+                />
+              </svg>
             </button>
           </template>
           <template v-else>
@@ -128,8 +155,16 @@ onMounted(() => {
                 editingTask = { id: task.id, content: task.taskContent }
               "
             >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1.2rem"><path fill="#B197FC" d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z"/></svg>
-
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                width="1.2rem"
+              >
+                <path
+                  fill="#B197FC"
+                  d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z"
+                />
+              </svg>
             </button>
             <button
               class="icon-btn"
@@ -137,7 +172,16 @@ onMounted(() => {
                 deleteConfirm = { id: task.id, content: task.taskContent }
               "
             >
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="1.2rem"><path fill="#a51d2d" d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                width="1.2rem"
+              >
+                <path
+                  fill="#a51d2d"
+                  d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"
+                />
+              </svg>
             </button>
           </template>
         </div>
@@ -224,6 +268,7 @@ onMounted(() => {
   display: flex;
   align-items: center;
   justify-content: space-between;
+  min-height: 3.5rem;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.08);
   transition: background-color 0.2s ease;
 }
@@ -234,6 +279,7 @@ onMounted(() => {
 
 .task-content {
   display: flex;
+  align-items: center;
   gap: 1rem;
   flex: 1;
 }
@@ -279,23 +325,18 @@ onMounted(() => {
 
 /* Checkbox Styles */
 .checkbox-container {
-  display: inline-block;
-  position: relative;
-  padding-left: 25px;
-  cursor: pointer;
-  font-size: 1rem;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 24px;
+  height: 24px;
 }
 
-.checkbox-container input {
-  position: absolute;
-  opacity: 0;
+.checkbox-container input[type="checkbox"] {
+  margin: 0;
+  width: 30px;
+  height: 30px;
   cursor: pointer;
-  height: 0;
-  width: 0;
 }
 
 .checkmark {
@@ -334,7 +375,7 @@ onMounted(() => {
   top: 3px;
   width: 5px;
   height: 10px;
-  border:2px solid #e74c3c;
+  border: 2px solid #e74c3c;
   border-width: 0 3px 3px 0;
   -webkit-transform: rotate(45deg);
   -ms-transform: rotate(45deg);
