144d8ade47cc9d0a9629af8c576d03ef27979600
Neil Vallecer <neilvallecer12@gmail.com>
2025-01-14 22:47:13 +0800

    refactor: Remove unused import from firebase-config and update tsconfig.app build info

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth/web-extension";
 import { initializeFirestore, CACHE_SIZE_UNLIMITED } from "firebase/firestore";
 import {
   getAuth,
