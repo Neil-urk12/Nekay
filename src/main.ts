3d0d7c63a8e446e11e7f63defc1aaeb8d9f182ba
Neil Vallecer <neilvallecer12@gmail.com>
2025-01-21 18:53:17 +0800

    fix: update service worker import path and change file extension to TypeScript

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from "pinia";
 import router from "./router";
 import "./style.css";
 import App from "./App.vue";
-import { registerServiceWorker } from "./utils/serviceWorker";
+import { registerServiceWorker } from "../src/utils/serviceWorker";
 
 interface IndexedDBError {
   code: string;
