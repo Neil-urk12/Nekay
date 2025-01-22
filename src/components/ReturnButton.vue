a83914d5e1af265d5a5d3ffb95c8c46c193c6e11
Neil Vallecer <neilvallecer12@gmail.com>
2025-01-22 23:53:22 +0800

    feat: Add ReturnButton component and integrate it into AllTasks and TaskView

---
+++ src/components/ReturnButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="back-btn" @click="router.push('/folders')">
+    ← Back to Folders
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+</script>
+
+<style scoped>
+.back-btn {
+  border: none;
+  cursor: pointer;
+  background: none;
+  font-size: 1rem;
+  position: absolute;
+  left: 0;
+  transition: color 0.2s ease;
+}
+
+.back-btn:hover {
+  color: #63e6be;
+}
+</style>
