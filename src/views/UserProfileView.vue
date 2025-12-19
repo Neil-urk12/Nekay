93111af741e6a0005fc1f66ac085be1c5dbdd804
Neil Vallecer <140867360+Neil-urk12@users.noreply.github.com>
2025-12-19 19:43:36 +0800

    chore: updated dependencies, installed minimatch to fix build error and fix linter issues

--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -3,11 +3,11 @@ import { Camera, Eye, EyeOff } from 'lucide-vue-next'
 import { computed, defineAsyncComponent, onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '../stores/authStore'
+import { useUserProfileStore } from '../stores/userProfileStore'
+import { supabase } from '../supabase/supabase-config'
 
 const SlideUpSheet = defineAsyncComponent(() => import('../components/SlideUpSheet.vue'))
 const UserAvatar = defineAsyncComponent(() => import('../components/UserAvatar.vue'))
-import { useUserProfileStore } from '../stores/userProfileStore'
-import { supabase } from '../supabase/supabase-config'
 
 const router = useRouter()
 const authStore = useAuthStore()
